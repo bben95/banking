3df1e3d236db581a0772b1bc36dfb3b285c82c04
bben95 <benakabp@gmail.com>
2025-05-19 14:44:31 +0530

    first commit

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,9 +4,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-  <main>
-    {children}
-  </main>
-
+    <main>
+      {children}
+      </main>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
-
-const Home = () => {
+export default function Home() {
   return (
-    <div>Home</div>
-  )
-}
+   <section className="home">
+    <div className="home-content">
+    <header className="">
+      <h1 className="text-success-600 text-xl">Welcome to the app</h1>  
+      Welcome,Adrian
+    </header>
+    </div>
+   </section>
 
-export default Home
+  );
+}
